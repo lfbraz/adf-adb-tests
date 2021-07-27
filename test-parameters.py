bb70984815d490b9a1de1e3e5e0dd8ccd4590387
Luiz Fernando Braz <lubraz@microsoft.com>
2021-07-27 11:35:33 -0300

    Add arguments function

--- test-parameters.py
+++ test-parameters.py
@@ -1,1 +1,11 @@
-print('test-parameters')
+import argparse
+
+def main(args):
+    print(f'My args:{args}')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--first_arg', type=str)
+    args = parser.parse_args()
+    main(args)
