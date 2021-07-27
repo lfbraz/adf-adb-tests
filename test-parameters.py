272927567ae7941495f491496e57e3f88cf49a67
Luiz Fernando Braz <lubraz@microsoft.com>
2021-07-27 12:44:04 -0300

    Change to parser

--- test-parameters.py
+++ test-parameters.py
@@ -1,12 +1,14 @@
 import argparse
 import sys
 
-def main(args):
+def main(args, args2):
     print(f'My args:{args}')
+    print(f'My args 2:{args2}')
 
 
 if __name__ == "__main__":
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument('--first_arg', type=str)
-    #args = parser.parse_args()
-    main(sys.argv)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', type=str)
+    parser.add_argument('--first_arg', type=str)
+    args = parser.parse_args()
+    main(sys.argv, args)
