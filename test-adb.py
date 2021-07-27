3c535f9fb20e5cd39201e58746c2f57df808a1a7
lfbraz <lfbraz>
2021-07-27 17:25:00 +0000

    Adapt do Databricks

--- test-adb.py
+++ test-adb.py
@@ -1,2 +1,15 @@
 # Databricks notebook source
-print('hello-world')
+import argparse
+import sys
+
+def main(args_sys, args_parser):
+    print(f'My args (using sys.argv):{args_sys[1]}')
+    print(f'My args (using argparse):{args_parser.first_arg}')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('first_arg', type=str)
+    args_parser = parser.parse_args()
+    main(sys.argv, args_parser)
+
