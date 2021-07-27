6016719e8b8b9c7f27460f2c8c1acc05c6ca287c
Luiz Fernando Braz <lubraz@microsoft.com>
2021-07-27 12:34:56 -0300

    Change to sys.argv

--- test-parameters.py
+++ test-parameters.py
@@ -1,11 +1,12 @@
 import argparse
+import sys
 
 def main(args):
     print(f'My args:{args}')
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--first_arg', type=str)
-    args = parser.parse_args()
-    main(args)
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('--first_arg', type=str)
+    #args = parser.parse_args()
+    main(sys.argv)
